0422968fb2bce329dcc570714db9e9144d5d8f62
ctrlshahida <shahidam.begum@hotmail.com>
2025-03-12 23:37:34 +0000

    Initial Commit

---
+++ Mosques/MosquesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MosquesApp.swift
+//  Mosques
+//
+//  Created by شهيدا on 12/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct MosquesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MosquesTests/MosquesTests.swift
@@ -0,0 +1,17 @@
+//
+//  MosquesTests.swift
+//  MosquesTests
+//
+//  Created by شهيدا on 12/03/2025.
+//
+
+import Testing
+@testable import Mosques
+
+struct MosquesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
